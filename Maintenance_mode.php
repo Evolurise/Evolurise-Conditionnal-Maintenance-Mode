3d1bbaf080e6ceef23fe16a887f639d46d7b9f46
Evolurise : Web Agency <122077963+Evolurise@users.noreply.github.com>
2023-02-26 17:51:42 +0100

    Multiple fixes
    Escaping options and variables, changing generic function/class/define/namespace names

--- Maintenance_mode.php
+++ Maintenance_mode.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Conditionnal Maintenance Mode for WordPress
+Plugin Name: Conditional Maintenance Mode for WordPress
 Plugin URI: https://www.evolurise.com/
 Description: Allows the administrator to enable or disable maintenance mode for selected user roles and customize the maintenance message.
 Version: 1.0.0
@@ -31,107 +31,101 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 // Hook the function to the 'wp_dashboard_setup' action
 add_action('wp_dashboard_setup', 'hide_dashboard_messages');
 
-
-add_action('wp_dashboard_setup', 'hide_dashboard_messages');
-
-add_action( 'admin_menu', 'mm_add_settings_page' );
-function mm_add_settings_page() {
-    add_options_page( 'Maintenance Mode', 'Maintenance Mode', 'manage_options', 'maintenance-mode', 'mm_settings_page' );
+add_action( 'admin_menu', 'maintenance_add_settings_page' );
+function maintenance_add_settings_page() {
+    add_options_page( esc_html__( 'Maintenance Mode', 'evolurise-maintenance-mode' ), esc_html__( 'Maintenance Mode', 'evolurise-maintenance-mode' ), 'manage_options', 'maintenance-mode', 'maintenance_settings_page' );
 }
 
 // Display the settings page
-function mm_settings_page() {
+function maintenance_settings_page() {
     wp_enqueue_style( 'basic-auth-for-wp-admin-style', plugin_dir_url( __FILE__ ) . 'styles_admin.css' );
     if ( !current_user_can( 'manage_options' ) )  {
-        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+        wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'evolurise-maintenance-mode' ) );
     }
     ?>
     <div class="wrap">
-    <img width="20%" src="<?php echo plugin_dir_url( __FILE__ ) . '/img/evolurise_logo.png'; ?>" alt="Evolurise logo">
-        <h1>Conditionnal Maintenance Mode based on Wordpress User roles </h1>
+    <img width="20%" src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . '/img/evolurise_logo.png' ); ?>" alt="<?php esc_attr_e( 'Evolurise logo', 'evolurise-maintenance-mode' ); ?>">
+        <h1><?php esc_html_e( 'Conditional Maintenance Mode based on WordPress User Roles', 'evolurise-maintenance-mode' ); ?></h1>
         <form method="post" action="options.php">
             <?php
-                settings_fields( 'mm_settings' );
+                settings_fields( 'maintenance_settings' );
                 do_settings_sections( 'maintenance-mode' );
                 submit_button();
             ?>
         </form>
-        <p>Thank you for using our plugin, please rate it and visit our website <a href="https://www.evolurise.com">evolurise.com</a></p>
+        <p><?php esc_html_e( 'Thank you for using our plugin, please rate it and visit our website', 'evolurise-maintenance-mode' ); ?> <a href="<?php echo esc_url( 'https://www.evolurise.com' ); ?>"><?php esc_html_e( 'evolurise.com', 'evolurise-maintenance-mode' ); ?></a></p>
     </div>
     <?php
 }
 
 // Register settings for the plugin
-add_action( 'admin_init', 'mm_register_settings' );
-function mm_register_settings() {
-    register_setting( 'mm_settings', 'mm_settings' );
-    add_settings_section( 'mm_maintenance_section', 'Conditionnal Maintenance Mode Settings Page ', 'mm_maintenance_section_callback', 'maintenance-mode' );
-    add_settings_field( 'mm_status', 'Activate the maintenance mode ?', 'mm_status_callback', 'maintenance-mode', 'mm_maintenance_section' );
-    add_settings_field( 'mm_roles', 'User Roles', 'mm_roles_callback', 'maintenance-mode', 'mm_maintenance_section' );
-    add_settings_field( 'mm_message', 'Maintenance Message', 'mm_message_callback', 'maintenance-mode', 'mm_maintenance_section' );
-}
-
-// Callback function for the maintenance mode section
-function mm_maintenance_section_callback() {
-    echo 'Welcome to this settings page of the conditionnal maintenance mode for Wordpress, if you have any question about the usage of our plugin please feel free to contact us at hello@evolurise.com';
-}
-
-// Callback function for the status field
-function mm_status_callback() {
-    $options = get_option( 'mm_settings' );
+add_action( 'admin_init', 'maintenance_register_settings' );
+function maintenance_register_settings() {
+    register_setting( 'maintenance_settings', 'maintenance_settings' );
+    add_settings_section( 'maintenance_maintenance_section', esc_html__('Conditional Maintenance Mode Settings Page', 'evolurise-maintenance-mode'), 'maintenance_maintenance_section_callback', 'maintenance-mode' );
+    add_settings_field( 'maintenance_status', esc_html__('Activate the maintenance mode?', 'evolurise-maintenance-mode'), 'maintenance_status_callback', 'maintenance-mode', 'maintenance_maintenance_section' );
+    add_settings_field( 'maintenance_roles', esc_html__('User Roles', 'evolurise-maintenance-mode'), 'maintenance_roles_callback', 'maintenance-mode', 'maintenance_maintenance_section' );
+    add_settings_field( 'maintenance_message', esc_html__('Maintenance Message', 'evolurise-maintenance-mode'), 'maintenance_message_callback', 'maintenance-mode', 'maintenance_maintenance_section' );
+    }
+    
+    // Callback function for the maintenance mode section
+    function maintenance_maintenance_section_callback() {
+    echo esc_html__('Welcome to this settings page of the conditional maintenance mode for WordPress. If you have any questions about the usage of our plugin, please feel free to contact us at hello@evolurise.com', 'evolurise-maintenance-mode');
+    }
+    
+    // Callback function for the status field
+    function maintenance_status_callback() {
+    $options = get_option( 'maintenance_settings' );
     $status = isset( $options['status'] ) ? $options['status'] : 'off';
     ?>
-    <select name="mm_settings[status]">
-        <option value="off" <?php selected( $status, 'off' ); ?>>Off</option>
-        <option value="on" <?php selected( $status, 'on' ); ?>>On</option>
+    <select name="maintenance_settings[status]">
+    <option value="off" <?php selected( $status, 'off' ); ?>><?php esc_html_e('Off', 'evolurise-maintenance-mode'); ?></option>
+    <option value="on" <?php selected( $status, 'on' ); ?>><?php esc_html_e('On', 'evolurise-maintenance-mode'); ?></option>
     </select>
     <?php
-}
-function show_maintenance_warning() {
-        $options = get_option( 'mm_settings' );
-        $status = isset( $options['status'] ) ? $options['status'] : 'off';
-        if ($status == 'on') {
-            echo '<div class="notice notice-error is-dismissible">';
-            echo '<p>Maintenance mode is currently <span style="color:green;font-weight:800;">active</span>. Only users with the appropriate roles will be able to access the site.</p>';
-            echo '</div>';
-        }
+    }
+    
+    function show_maintenance_warning() {
+    $options = get_option( 'maintenance_settings' );
+    $status = isset( $options['status'] ) ? $options['status'] : 'off';
+    if ($status == 'on') {
+    echo '<div class="notice notice-error is-dismissible">';
+    echo '<p>' . esc_html__('Maintenance mode is currently', 'evolurise-maintenance-mode') . '<span style="color:green;font-weight:800;"> ' . esc_html__('active', 'evolurise-maintenance-mode') . '</span>. ' . esc_html__('Only users with the appropriate roles will be able to access the site.', 'evolurise-maintenance-mode') . '</p>';
+    echo '</div>';
+    }
     }
     add_action( 'admin_notices', 'show_maintenance_warning' );
-
-
-// Callback function for the roles field
-function mm_roles_callback() {
-    $options = get_option( 'mm_settings' );
-    $roles=isset( $options['roles'] ) ? $options['roles'] : array();
+    
+    // Callback function for the roles field
+    function maintenance_roles_callback() {
+    $options = get_option( 'maintenance_settings' );
+    $roles = isset( $options['roles'] ) ? $options['roles'] : array();
     global $wp_roles;
     foreach ( $wp_roles->roles as $role => $details ) {
-    $name = translate_user_role($details['name'] );
+    $name = translate_user_role( $details['name'] );
     ?>
-    <input type="checkbox" name="mm_settings[roles][]" value="<?php echo $role; ?>" <?php checked( in_array( $role, $roles ) ); ?>> <?php echo $name; ?><br>
+    <input type="checkbox" name="maintenance_settings[roles][]" value="<?php echo esc_attr( $role ); ?>" <?php checked( in_array( $role, $roles ) ); ?>> <?php echo esc_html( $name ); ?><br>
     <?php
     }
-    // adding guest checkbox
+    }
+    
+// Callback function for the message field
+function maintenance_message_callback() {
+    $options = get_option( 'maintenance_settings' );
+    $message = isset( $options['message'] ) ? $options['message'] : '';
     ?>
-    <input type="checkbox" name="mm_settings[roles][]" value="guest" <?php checked( in_array( 'guest', $roles ) ); ?>> Invité<br>
+    <textarea name="maintenance_settings[message]" rows="5" cols="50"><?php echo esc_textarea( sanitize_textarea_field( $message ) ); ?></textarea>
     <?php
 }
-    
-    // Callback function for the message field
-    function mm_message_callback() {
-    $options = get_option( 'mm_settings' );
-    $message = isset( $options['message'] ) ? $options['message'] : 'Sorry, we are currently undergoing maintenance. Please check back later.';
-    ?>
-    <textarea name="mm_settings[message]" rows="5" cols="50"><?php echo $message; ?></textarea>
-    <?php
-    }
+
     
 // Redirect users to the maintenance page if maintenance mode is on
-add_action( 'template_redirect', 'mm_maintenance_redirect' );
-function mm_maintenance_redirect() {
-$options = get_option( 'mm_settings' );
-$status = isset( $options['status'] ) ? $options['status'] : 'off';
-$roles = isset( $options['roles'] ) ? $options['roles'] : array();
-$message = isset( $options['message'] ) ? $options['message'] : 'Sorry, we are currently undergoing maintenance. Please check back later.';
+add_action( 'template_redirect', 'maintenance_maintenance_redirect' );
+function maintenance_maintenance_redirect() {
+$options = get_option( 'maintenance_settings' );
+$status = isset( $options['status'] ) ? esc_attr( $options['status'] ) : 'off';
+$roles = isset( $options['roles'] ) ? array_map( 'esc_attr', $options['roles'] ) : array();
+$message = isset( $options['message'] ) ? esc_html( $options['message'] ) : 'Sorry, we are currently undergoing maintenance. Please check back later.';
 if ( $status == 'on' && !empty( $roles ) ) {
 $current_user = wp_get_current_user();
 $user_role = $current_user->roles[0];
@@ -139,29 +133,30 @@ if ( in_array( $user_role, $roles ) ) {
 wp_die( $message );
 }
 if (in_array('guest', $roles) && !is_user_logged_in()) {
-    wp_die($message);
+wp_die($message);
 }
 }
 }
 
+
 // Add a toggle link to the top admin bar
-add_action( 'admin_bar_menu', 'mm_admin_bar_menu', 999 );
-function mm_admin_bar_menu( $wp_admin_bar ) {
+add_action( 'admin_bar_menu', 'maintenance_admin_bar_menu', 999 );
+function maintenance_admin_bar_menu( $wp_admin_bar ) {
     if ( !current_user_can( 'manage_options' ) ) {
         return;
     }
 
-    $options = get_option( 'mm_settings' );
+    $options = get_option( 'maintenance_settings' );
     $status = isset( $options['status'] ) ? $options['status'] : 'off';
 
     if ( $status == 'on' ) {
-        $class = 'mm-on';
+        $class = 'maintenance-on';
         $title = 'Maintenance Mode: <span style="background-color:Green;color:white;border-radius:30%;padding:2px 5px;font-weight:600;">On</span>';
-        $href = add_query_arg( 'mm-status', 'off' );
+        $href = add_query_arg( 'maintenance-status', 'off' );
     } else {
-        $class = 'mm-off';
+        $class = 'maintenance-off';
         $title = 'Maintenance Mode: <span style="background-color:gray;color:white;border-radius:30%;padding:2px 5px;font-weight:600;">Off</span>';
-        $href = add_query_arg( 'mm-status', 'on' );
+        $href = add_query_arg( 'maintenance-status', 'on' );
     }
 
     $args = array(
@@ -174,23 +169,24 @@ function mm_admin_bar_menu( $wp_admin_bar ) {
 }
 
 // Handle the toggle link from the admin bar
-add_action( 'admin_init', 'mm_admin_bar_toggle' );
-function mm_admin_bar_toggle() {
+add_action( 'admin_init', 'maintenance_admin_bar_toggle' );
+function maintenance_admin_bar_toggle() {
     if ( !current_user_can( 'manage_options' ) ) {
         return;
     }
 
-    if ( !isset( $_GET['mm-status'] ) ) {
+    if ( !isset( $_GET['maintenance-status'] ) ) {
         return;
     }
 
-    $options = get_option( 'mm_settings' );
+    $options = get_option( 'maintenance_settings' );
 
-    if ( $_GET['mm-status'] == 'on' ) {
+    if ( $_GET['maintenance-status'] == 'on' ) {
         $options['status'] = 'on';
     } else {
         $options['status'] = 'off';
     }
 
-    update_option( 'mm_settings', $options );
+    update_option( 'maintenance_settings', $options );
 }
+
